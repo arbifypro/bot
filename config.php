b3ae1952730ff71fbb4cd746406c034ce0a2fa10
Alexandr Yemelianov <fcdnipro268@gmail.com>
2025-03-22 17:13:36 +0100

    add db

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 const BOT_TOKEN = "7635278801:AAGA1mtSyxjUazxNqA-1wHAVAD7PTo_KSXE";
 //const API_KEY = "7635278801:AAGA1mtSyxjUazxNqA-1wHAVAD7PTo_KSXE";
-const DB_HOST = "mysql://root:XPmBUcIFVTJogvzmUfuXwbmRRxLmzAks@hopper.proxy.rlwy.net:30625/railway";
+const DB_HOST = "hopper.proxy.rlwy.net:30625";
 const DB_PORT = "3306";
 const DB_NAME = "railway";
 const DB_USER = "root";

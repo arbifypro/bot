ed881c57f7f307b0bcca9444b29894688cb39bc9
Alexandr Yemelianov <fcdnipro268@gmail.com>
2025-04-28 20:51:29 +0200

    add new doc

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php
 
 const BOT_TOKEN = "7639727556:AAFQhCd2K5J3g77A0K4B2k1gv9awanB0-vw";
-const DB_HOST = "hopper.proxy.rlwy.net:30625";
+const DB_HOST = "trolley.proxy.rlwy.net:49064";
+
 const DB_PORT = "3306";
 const DB_NAME = "railway";
 const DB_USER = "root";
-const DB_PASSWORD = "XPmBUcIFVTJogvzmUfuXwbmRRxLmzAks";
+const DB_PASSWORD = "SMYRyoJdXELgZxZCAUMVLtIeidipsJzr";

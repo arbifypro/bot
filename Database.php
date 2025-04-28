4528ce2a1e229208a5539686c69329da85102014
Alexandr Yemelianov <fcdnipro268@gmail.com>
2025-04-28 20:40:22 +0200

    add new doc

--- Database.php
+++ Database.php
@@ -6,6 +6,7 @@ class Database
 
     public function __construct($config)
     {
+        $config = $config['db'];
         $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset={$config['charset']}";
         $this->pdo = new PDO($dsn, $config['username'], $config['password'], [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,

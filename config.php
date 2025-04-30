82a6ffcb9f6f24a525cb3075570c44fc72b9aa21
Alexandr Yemelianov <fcdnipro268@gmail.com>
2025-04-30 09:40:49 +0200

    add new doc

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-const BOT_TOKEN = "7639727556:AAFQhCd2K5J3g77A0K4B2k1gv9awanB0-vw";
+define("BOT_TOKEN", getenv('TELEGRAM_BOT_TOKEN'));
 const DB_HOST = "trolley.proxy.rlwy.net:49064";
 const DB_PORT = "3306";
 const DB_NAME = "railway";
